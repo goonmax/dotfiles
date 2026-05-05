c3b782bb426e7d21c64e71e91f1377bd54814bb2
Che <chetynan@hotmail.com>
2026-05-05 12:01:02 +1200

    fixe terminal moved

--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -22,7 +22,7 @@ vim.keymap.set("n", "<leader>vrc", ":edit ~/.config/nvim/<CR>", { silent = true 
 vim.keymap.set("n", "<C-l>", ':nohl<CR><C-l>:echo "Search Cleared"<CR>', { silent = true })
 vim.keymap.set("n", "<C-c>", ':set norelativenumber nonumber<CR>:echo "Line numbers off"<CR>', { silent = true })
 vim.keymap.set("n", "<C-n>", ':set number relativenumber<CR>:echo "Line numbers on"<CR>', { silent = true })
-vim.keymap.set("n", "<Esc>", "<C-\\><C-n>", { silent = true })
+vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", { silent = true })
 vim.keymap.set("n", "<leader>m", ":!mkdir -p %:h<CR>", { silent = true })
 vim.keymap.set("n", "<leader>b", ":ls<CR>:b<Space>", { silent = false })
 vim.keymap.set("n", "<leader>lf", vim.lsp.buf.format)
